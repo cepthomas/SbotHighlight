fb00b70d86e665e578bbb9aaad2667591b505ea0
cepthomas <cepthomas@gmail.com>
2024-07-22 17:38:36 -0500

    Playing with logging

--- sbot_highlight.py
+++ sbot_highlight.py
@@ -18,6 +18,23 @@ _hls = {}
 
 
 #-----------------------------------------------------------------------------------
+def plugin_loaded():
+    ''' Called once per plugin instance. '''
+
+    # Set up logging.
+    _logger = sc.init_log(__package__)
+    print(f'>>> plugin_loaded() {__package__} {id(_logger)}')
+
+
+#-----------------------------------------------------------------------------------
+def plugin_unloaded():
+    ''' Called once per plugin instance. '''
+
+    # Clean up logging.
+    sc.deinit_log(_logger)
+
+
+#-----------------------------------------------------------------------------------
 class HighlightEvent(sublime_plugin.EventListener):
 
     # Need to track what's been initialized.
